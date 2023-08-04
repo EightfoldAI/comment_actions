816ea0f4175e70817caea26a7eee681ceef9e762
Aniket Patanwal <76991980+apatanwal-eightfold@users.noreply.github.com>
2023-08-04 11:53:11 +0530

    Revert "Add Python version 3.11" (#3)

--- entrypoint.sh
+++ entrypoint.sh
@@ -56,10 +56,8 @@ echo $number
 echo $labels
 
 already_needs_ci=false
-already_needs_ci_3_11=false
 already_shipit=false
 already_verified=false
-already_verified_3_11=false
 
 if [[ "$action" != "created" ]]; then
   echo This action should only be called when a comment is created on a pull request
@@ -72,17 +70,11 @@ if [[ $comment_body == "shipit" || $comment_body == ":shipit:" || $comment_body 
       ci_verified)
         already_verified=true
         ;;
-      "ci_verified:3.11")
-        already_verified_3_11=true
-        ;;
       shipit)
         already_shipit=true
         ;;
       needs_ci)
         already_needs_ci=true
-        ;;
-      "needs_ci:3.11")
-        already_needs_ci_3_11=true
         ;;
       *)
         echo "Unknown label $label"
@@ -91,9 +83,6 @@ if [[ $comment_body == "shipit" || $comment_body == ":shipit:" || $comment_body 
   done
   if [[ "$already_verified" == false && "$already_needs_ci" == false ]]; then
     add_label "needs_ci"
-  fi
-  if [[ "$already_verified_3_11" == false && "$already_needs_ci_3_11" == false ]]; then
-    add_label "needs_ci:3.11"
   fi
   if [[ "$already_shipit" == false ]]; then
     add_label "shipit"
@@ -120,28 +109,6 @@ if [[ $comment_body == "needs_ci" ]]; then
   done
   if [[ "$already_needs_ci" == false ]]; then
     add_label "needs_ci"
-  fi
-fi
-
-if [[ $comment_body == "needs_ci:3.11" ]]; then
-  for label in $labels; do
-    case $label in
-      "ci_verified:3.11")
-        remove_label "$label"
-        ;;
-      shipit)
-        remove_label "$label"
-        ;;
-      "needs_ci:3.11")
-        already_needs_ci_3_11=true
-        ;;
-      *)
-        echo "Unknown label $label"
-        ;;
-    esac
-  done
-  if [[ "$already_needs_ci_3_11" == false ]]; then
-    add_label "needs_ci:3.11"
   fi
 fi
 
